c9e87a1b7d95ba27e322389aae577f1ae0053683
Divya <divyabaheti101@gmail.com>
2024-01-08 21:37:06 +0530

    defined routes, BrowserRouter, Link, Routes, Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import { Router, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-<div>
-  <nav>
-    <ul>
-      <li>
-        <Link to='/'>Home</Link>
-      </li>
-      <li>
-        <Link to='/about'>About</Link>
-      </li>
-    </ul>
-  </nav>
-</div>
-    </Router>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
+
   );
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function NotFound() {
+  return <h2>404 Not Found</h2>
 }
 
 export default App;
